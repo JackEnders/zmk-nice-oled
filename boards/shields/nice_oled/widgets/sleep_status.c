792ab86bfd15ddb7f8b0d4fa2396bed82050fd19
Jack Enders <jack@enders.sh>
2026-06-24 00:50:21 -0300

    sleep art: big vertical crocodile on both screens
    - New croc_sleep asset (160x68 full-screen, big vertical croc).
    - sleep_status.c uses croc_sleep on both central and peripheral.
    - Fix upstream bugs that made sleep art never show: the show/hide logic
      gated on CONFIG_NICE_PERI_VIEW_SHOW_SLEEP_ART_* (not a real symbol) ->
      use CONFIG_NICE_OLED_SHOW_SLEEP_ART_*; and the CMake guard checked
      ON_IDLE twice -> OR ON_SLEEP. Hide the art at init so it only appears
      on idle/sleep.

--- boards/shields/nice_oled/widgets/sleep_status.c
+++ boards/shields/nice_oled/widgets/sleep_status.c
@@ -20,6 +20,7 @@ static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 LV_IMG_DECLARE(vim_32x128);
 LV_IMG_DECLARE(vim_68x160);
 LV_IMG_DECLARE(sleep_oled);
+LV_IMG_DECLARE(croc_sleep);
 
 static void set_sleep_img(struct zmk_widget_sleep_status *widget,
                           struct zmk_activity_state_changed ev) {
@@ -30,7 +31,7 @@ static void set_sleep_img(struct zmk_widget_sleep_status *widget,
         break;
     case ZMK_ACTIVITY_IDLE:
         LOG_DBG("ACTIVITY EVENT IDLE");
-#if IS_ENABLED(CONFIG_NICE_PERI_VIEW_SHOW_SLEEP_ART_ON_IDLE)
+#if IS_ENABLED(CONFIG_NICE_OLED_SHOW_SLEEP_ART_ON_IDLE)
         lv_obj_clear_flag(widget->art, LV_OBJ_FLAG_HIDDEN);
 #else
         lv_obj_add_flag(widget->art, LV_OBJ_FLAG_HIDDEN);
@@ -38,7 +39,7 @@ static void set_sleep_img(struct zmk_widget_sleep_status *widget,
         break;
     case ZMK_ACTIVITY_SLEEP:
         LOG_DBG("ACTIVITY EVENT SLEEP");
-#if IS_ENABLED(CONFIG_NICE_PERI_VIEW_SHOW_SLEEP_ART_ON_SLEEP)
+#if IS_ENABLED(CONFIG_NICE_OLED_SHOW_SLEEP_ART_ON_SLEEP)
         lv_obj_clear_flag(widget->art, LV_OBJ_FLAG_HIDDEN);
 #else
         lv_obj_add_flag(widget->art, LV_OBJ_FLAG_HIDDEN);
@@ -70,14 +71,14 @@ int zmk_widget_sleep_status_init(struct zmk_widget_sleep_status *widget, lv_obj_
     lv_obj_set_size(widget->obj, 160, 68);
     widget->art = lv_img_create(widget->obj);
 #if IS_ENABLED(CONFIG_NICE_EPAPER_ON)
-    lv_img_set_src(widget->art, &vim_68x160);
+    lv_img_set_src(widget->art, &croc_sleep);
 #else
     // lv_img_set_src(widget->art, &sleep_oled);
-    lv_img_set_src(widget->art, &vim_32x128);
+    lv_img_set_src(widget->art, &croc_sleep);
 #endif
     lv_obj_align(widget->art, LV_ALIGN_TOP_LEFT, 0, 0);
 
-    // lv_obj_add_flag(widget->art, LV_OBJ_FLAG_HIDDEN);
+    lv_obj_add_flag(widget->art, LV_OBJ_FLAG_HIDDEN);
 
     widget_sleep_status_init();
     return 0;
